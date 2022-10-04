69ea7058bd2995cd36dcd57a9646dfdbdcf74a5d
GuYan1024 <chinaguyan@gmail.com>
2022-10-04 22:45:29 +0800

    update blocks.xlsx

--- database/sample/getNameFromJson.js
+++ database/sample/getNameFromJson.js
@@ -1,13 +1,18 @@
 function getNameFromJson(json) {
+  document.write("<ul>")
   var project = JSON.parse(json);
   let targets = project["targets"];
   for (let i = 0; i < targets.length; i++) {
     let sprite = targets[i];
     let blocks = sprite["blocks"];
     for (key in blocks) {
-      console.log(blocks[key]["opcode"].split("_")[1]);
+      let opcode = blocks[key]["opcode"];
+      document.write(
+        "<li>" + opcode.slice(opcode.indexOf("_") + 1, opcode.length) + "</li>"
+      );
     }
   }
+  document.write("</ul>")
 }
 
 json = prompt("Input Json: ");
